70c6ff9651504aee07ebd805c035b33677eab441
ivan.cherednychok <ravado@hotmail.com>
2025-08-06 18:36:42 +0300

    Synced Batanovy pictureframe scripts

--- PhotoFrame/monitor_control.sh
+++ PhotoFrame/monitor_control.sh
@@ -3,7 +3,7 @@
 # We don't want this to make the frame working at not desired times
 
 # Set your 'turn off' and 'turn on' times in "HH:MM" format (don't forget to sync with crontab)
-TURN_OFF_TIME="21:00"
+TURN_OFF_TIME="23:00"
 TURN_ON_TIME="07:00"
 
 # Convert times to minutes since midnight for easy comparison
@@ -18,14 +18,12 @@ CURRENT_TIME=$(date +"%H:%M")
 CURRENT_MINUTES=$(convert_to_minutes "$CURRENT_TIME")
 
 # Check if current time is outside the active period
-# Set X env vars in case xset is used
-export DISPLAY=:0
 if [ "$CURRENT_MINUTES" -ge "$TURN_OFF_MINUTES" ] || [ "$CURRENT_MINUTES" -lt "$TURN_ON_MINUTES" ]; then
-    echo "🔌 Turning OFF monitor at $CURRENT_TIME"
-    vcgencmd display_power 0 2>/dev/null
-    xset dpms force off 2>/dev/null
+    # Command to turn off the monitor
+    echo "Turning off monitor at $CURRENT_TIME"
+    vcgencmd display_power 0
 else
-    echo "💡 Turning ON monitor at $CURRENT_TIME"
-    vcgencmd display_power 1 2>/dev/null
-    xset dpms force on 2>/dev/null
+    # Command to turn on the monitor
+    echo "Turning on monitor at $CURRENT_TIME"
+    vcgencmd display_power 1
 fi
